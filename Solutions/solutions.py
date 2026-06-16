40bdfc6068581a50fc96be512a5f1f1824fce4c6
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-06-16 16:29:29 +0530

    Merge Two Sorted Arrays

--- Solutions/solutions.py
+++ Solutions/solutions.py
@@ -214,3 +214,26 @@ def repeating(arr):
         s.add(i)
     return len(s)
 print(repeating([1,2,3,4,5,6,1,2]))
+
+
+# Merge Two Sorted Arrays
+def merge_sorted_arrays(arr1, arr2):
+    i = 0
+    j = 0
+    result = []
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            result.append(arr1[i])
+            i+=1
+        else:
+            result.append(arr2[j])
+            j+=1
+    
+    while i < len(arr1):
+        result.append(arr1[i])
+        i+=1
+    while j < len(arr2):
+        result.append(arr2[j])
+        j+=1
+    return result
+print(merge_sorted_arrays([1, 2, 3, 5, 7], [4, 6]))
