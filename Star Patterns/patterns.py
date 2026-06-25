a80e16758b9bb8a8803c9b824d4f89e8a01daa23
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-06-25 15:30:56 +0530

    Add additional star pattern generation scripts for various shapes

--- Star Patterns/patterns.py
+++ Star Patterns/patterns.py
@@ -54,3 +54,42 @@ for i in range(5):
         pattern += "* "
     print(pattern)
 
+
+
+#           * 
+#         * * 
+#       * * * 
+#     * * * * 
+#   * * * * * 
+
+n = 5
+for i in range(n):
+    pattern = ""
+    for j in range(i, n):
+        pattern += "  "
+    for k in range(i+1):
+        pattern += "* "
+    print(pattern)
+#      * 
+#     * * 
+#    * * * 
+#   * * * * 
+#  * * * * * 
+# AND FOR THIS PATTERN JUST REMOVE ONE SPACE FROM ABOVE PATTERN J LOOP
+
+#           * 
+#         * * * 
+#       * * * * * 
+#     * * * * * * * 
+#   * * * * * * * * *
+
+    n = 5
+for i in range(n):
+    pattern = ""
+    for j in range(i, n):
+        pattern += "  "
+    for k in range(i+1):
+        pattern += "* "
+    for l in range(i):
+        pattern += "* "
+    print(pattern)
