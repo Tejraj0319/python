7a7c6fb9629c377deb2b3e345af72851dae7af53
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-08-17 13:53:37 +0530

    Refactor pattern generation code for clarity; update comments and formatting

--- Star Patterns/patterns.py
+++ Star Patterns/patterns.py
@@ -11,25 +11,25 @@ for i in range(n):
         pattern += "* "
     print(pattern)
 
-# * 
-# * * 
-# * * * 
-# * * * * 
-# * * * * * 
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
 
 n = 5
 for i in range(n):
     pattern = ""
-    for j in range(i+1):
+    for j in range(i + 1):
         pattern += "* "
     print(pattern)
 
 
-# * * * * * 
-# * * * * 
-# * * * 
-# * * 
-# * 
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
 
 n = 5
 for i in range(n):
@@ -39,57 +39,72 @@ for i in range(n):
     print(pattern)
 
 
-#   * * * * * 
-#     * * * * 
-#       * * * 
-#         * * 
-#           * 
+#   * * * * *
+#     * * * *
+#       * * *
+#         * *
+#           *
 
 n = 5
 for i in range(5):
     pattern = ""
-    for j in range(i+1):
+    for j in range(i + 1):
         pattern += "  "
-    for k in range(i,n):
+    for k in range(i, n):
         pattern += "* "
     print(pattern)
 
 
-
-#           * 
-#         * * 
-#       * * * 
-#     * * * * 
-#   * * * * * 
+#           *
+#         * *
+#       * * *
+#     * * * *
+#   * * * * *
 
 n = 5
 for i in range(n):
     pattern = ""
     for j in range(i, n):
         pattern += "  "
-    for k in range(i+1):
+    for k in range(i + 1):
         pattern += "* "
     print(pattern)
-#      * 
-#     * * 
-#    * * * 
-#   * * * * 
-#  * * * * * 
-# AND FOR THIS PATTERN JUST REMOVE ONE SPACE FROM ABOVE PATTERN J LOOP
+    #      *
+    #     * *
+    #    * * *
+    #   * * * *
+    #  * * * * *
+    # AND FOR THIS PATTERN JUST REMOVE ONE SPACE FROM ABOVE PATTERN J LOOP
 
-#           * 
-#         * * * 
-#       * * * * * 
-#     * * * * * * * 
-#   * * * * * * * * *
+    #           *
+    #         * * *
+    #       * * * * *
+    #     * * * * * * *
+    #   * * * * * * * * *
 
     n = 5
 for i in range(n):
     pattern = ""
     for j in range(i, n):
         pattern += "  "
-    for k in range(i+1):
+    for k in range(i + 1):
         pattern += "* "
     for l in range(i):
         pattern += "* "
+    print(pattern)
+
+
+# * * * * *
+# *       *
+# *       *
+# *       *
+# * * * * *
+n = 5
+for i in range(n):
+    pattern = ""
+    for j in range(n):
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            pattern += "* "
+        else:
+            pattern += "  "
     print(pattern)
